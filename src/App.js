63c0a18f98efee33cc00c20129d32c8e76d54d97
Miklovsh <miklovsh.vik@gmail.com>
2023-02-01 19:12:36 +0200

    Created a layout for the Winner component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Start from './Start';
 import Categories from './Categories';
 import Quiz from './Quiz';
+import Winner from './Winner';
 
 function App() {
   return (
     <div className="App">
       {/* <Start /> */}
-      <div className="studio">
+      {/* <div className="studio">
         <div className="studio__monitor">
-          {/* <Categories /> */}
-          <Quiz />
+          <Categories /> 
+           <Quiz /> 
         </div>
-      </div>
+      </div> */}
+      <Winner />
     </div>
   );
 }
---
+++ src/Winner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import WinnerBG from "./fireworks-bg.mp4";
+import Award from "./award.png";
+
+
+function Winner() {
+  return (
+    <div className="winner" >
+      <div className="winner__video">
+        <video className="winner__video-bg" src={WinnerBG} autoPlay muted loop> </video>
+      </div>
+      <div className="winner__content">
+        <h1 className="winner__title">Вітаю Імя ви стали переможцем гри "Розумник"</h1>
+        <img className="winner__award" src={Award} />
+        <p className="winner__result">Ваш результат - 27 правильних відповідей</p>
+      </div>
+    </div>
+  )
+}
+
+export default Winner;
