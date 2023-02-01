c32a9f1385d853cfe3a6c3296edb6b476c86048d
Miklovsh <miklovsh.vik@gmail.com>
2023-02-02 00:56:55 +0200

    Added layout for message if you lost

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Quiz from './Quiz';
 import Winner from './Winner';
 
 function App() {
+
   return (
     <div className="App">
       {/* <Start /> */}
-      {/* <div className="studio">
+      <div className="studio">
+        <span className="studio__result">Результат: 10 відповідей</span>
         <div className="studio__monitor">
-          <Categories /> 
-           <Quiz /> 
+          {/* <Categories /> 
+           <Quiz />  */}
+          <div className="studio__message">
+            <h4 className="studio__message-title">Ви програли!</h4>
+            <span className="studio__message-result">Ваш результат: 5 відповідей</span>
+            <button className="studio__message-btn">Який був приз за перемогу?</button>
+          </div>
         </div>
-      </div> */}
-      <Winner />
+      </div>
+      {/* <Winner /> */}
     </div>
   );
 }
